f4cfd2144d24aae905437f00dfce6cf61c1b2bf2
Sveta2507 <dnsdns.dn@googlemail.com>
2020-07-28 01:26:04 +0300

    js

--- homeworkAll.js
+++ homeworkAll.js
@@ -462,113 +462,113 @@
  * Типов транзацкий всего два.
  * Можно положить либо снять деньги со счета.
  */
-const Transaction = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
-};
+// const Transaction = {
+//   DEPOSIT: 'deposit',
+//   WITHDRAW: 'withdraw',
+// };
 
-/*
- * Каждая транзакция это объект со свойствами: id, type и amount
- */
+// /*
+//  * Каждая транзакция это объект со свойствами: id, type и amount
+//  */
 
-const account = {
-  // Текущий баланс счета
-  id: 0,
-  balance: 0,
+// const account = {
+//   // Текущий баланс счета
+//   id: 0,
+//   balance: 0,
 
-  // История транзакций
-  transactions: [],
+//   // История транзакций
+//   transactions: [],
 
-  /*
-   * Метод создает и возвращает объект транзакции.
-   * Принимает сумму и тип транзакции.
-   */
-  createTransaction(amount, type) {
-    this.id += 1;
-    return {
-      id: this.id,
-      amount: amount,
-      type: type,
-    }
-  },
+//   /*
+//    * Метод создает и возвращает объект транзакции.
+//    * Принимает сумму и тип транзакции.
+//    */
+//   createTransaction(amount, type) {
+//     this.id += 1;
+//     return {
+//       id: this.id,
+//       amount: amount,
+//       type: type,
+//     }
+//   },
 
-  /*
-   * Метод отвечающий за добавление суммы к балансу.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций
-   */
-  deposit(amount) {
-    this.balance += amount;
-    let newTransaction = this.createTransaction(amount, Transaction.DEPOSIT)
-    this.transactions.push(newTransaction);
-  },
+//   /*
+//    * Метод отвечающий за добавление суммы к балансу.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций
+//    */
+//   deposit(amount) {
+//     this.balance += amount;
+//     let newTransaction = this.createTransaction(amount, Transaction.DEPOSIT)
+//     this.transactions.push(newTransaction);
+//   },
 
-  /*
-   * Метод отвечающий за снятие суммы с баланса.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций.
-   *
-   * Если amount больше чем текущий баланс, выводи сообщение
-   * о том, что снятие такой суммы не возможно, недостаточно средств.
-   */
-  withdraw(amount) {
-    if (this.balance < amount) {
-      alert("У Вас не достаточно денег");
-    } else {
-      this.balance -= amount;
-      let newTransaction = this.createTransaction(amount, Transaction.WITHDRAW)
-      this.transactions.push(newTransaction);
-    }
-  },
+//   /*
+//    * Метод отвечающий за снятие суммы с баланса.
+//    * Принимает сумму танзакции.
+//    * Вызывает createTransaction для создания объекта транзакции
+//    * после чего добавляет его в историю транзакций.
+//    *
+//    * Если amount больше чем текущий баланс, выводи сообщение
+//    * о том, что снятие такой суммы не возможно, недостаточно средств.
+//    */
+//   withdraw(amount) {
+//     if (this.balance < amount) {
+//       alert("У Вас не достаточно денег");
+//     } else {
+//       this.balance -= amount;
+//       let newTransaction = this.createTransaction(amount, Transaction.WITHDRAW)
+//       this.transactions.push(newTransaction);
+//     }
+//   },
 
-  /*
-   * Метод возвращает текущий баланс
-   */
-  getBalance() {
-    return this.balance;
-  },
+//   /*
+//    * Метод возвращает текущий баланс
+//    */
+//   getBalance() {
+//     return this.balance;
+//   },
 
-  /*
-   * Метод ищет и возвращает объект транзации по id
-   */
-  getTransactionDetails(id) {
-    for (let transaction of this.transactions) {
-      if (transaction.id === id) {
-        return transaction;
-      }
-    }
-  },
+//   /*
+//    * Метод ищет и возвращает объект транзации по id
+//    */
+//   getTransactionDetails(id) {
+//     for (let transaction of this.transactions) {
+//       if (transaction.id === id) {
+//         return transaction;
+//       }
+//     }
+//   },
 
-  /*
-   * Метод возвращает количество средств
-   * определенного типа транзакции из всей истории транзакций
-*/
-getTransactionTotal(type) {
-  let total = 0;
-  for (let transaction of this.transactions) {
-    if (transaction.type === type) {
-      total += transaction.amoumt;
-    }
-  }
-  return total;
-},
-};
+//   /*
+//    * Метод возвращает количество средств
+//    * определенного типа транзакции из всей истории транзакций
+// */
+// getTransactionTotal(type) {
+//   let total = 0;
+//   for (let transaction of this.transactions) {
+//     if (transaction.type === type) {
+//       total += transaction.amoumt;
+//     }
+//   }
+//   return total;
+// },
+// };
 
-account.deposit(60);
-console.log(account.getBalance());
-console.log(account.transactions);
-account.deposit(100);
-account.deposit(200);
-account.deposit(300);
-account.deposit(400);
-account.deposit(500);
-account.deposit(600);
-console.log(account.getBalance());
-console.log(account.transactions);
+// account.deposit(60);
+// console.log(account.getBalance());
+// console.log(account.transactions);
+// account.deposit(100);
+// account.deposit(200);
+// account.deposit(300);
+// account.deposit(400);
+// account.deposit(500);
+// account.deposit(600);
+// console.log(account.getBalance());
+// console.log(account.transactions);
 
-account.withdraw(3000);
-console.log(account.getBalance());
-console.log(account.transactions);
-console.log(account.getTransactionDetails(5));
+// account.withdraw(3000);
+// console.log(account.getBalance());
+// console.log(account.transactions);
+// console.log(account.getTransactionDetails(5));
--- module4.js
+++ module4.js
@@ -42,7 +42,6 @@ const fruits = [
     { name: "oranges" , quantity: 120 }, 
 ];    
 
-function filterFresh(); 
 const filterFruits = function (array, cbFunc) {
     const filterArray = []; 
     for(let element of array){ 
@@ -50,11 +49,80 @@ const filterFruits = function (array, cbFunc) {
         const ok = cbFunc(element);
         console.log(ok); 
         if(ok) {
-            filterArray.push(elenent); 
+            filterArray.push(element); 
         }
     };
     return console.log(filterArray);
 }
 filterFruits(fruits, (fresh) => fresh.isFresh);
 filterFruits(fruits, (el) => el.quantity > 100);
+ 
 
+//колбэки
+// stack вызов
+
+function main(param) {
+    console.log("Начала работать главная функция");
+    param();
+} 
+
+function cb() {
+    console.log("Выполняется колбэк функция");
+}
+main(cb);
+
+
+// замыкание
+function createCounter() {
+    let value = 0;
+    function increment () {
+        value += 1;
+        return console.log(value);
+    }
+    function decrement () {
+        value -= 1;
+        return console.log(value);
+    }
+    return {increment, decrement}
+}
+const counter1 = createCounter();
+counter1.increment();
+
+counter1.increment();
+counter1.increment();
+
+
+// ключевое слово this
+const nam = {
+    number: 0,
+    increment() {
+        return this.number+=1;
+    }
+}
+console.log(nam.increment());
+
+//или так
+
+// let number = 10;
+// const nam = {
+//     number: 0,
+//     increment() {
+//         return number+=1;
+//     }
+// }
+// console.log(nam.increment());
+
+// методы call и apply
+function hello(hotel, stars) {
+    return console.log(`hello, ${this.name}, ${hotel}, ${stars}, I'm glad to see you`);
+}
+
+const user = {
+    name: "sveta",
+}
+hello.call(user, "Rixos", "five stars");
+
+hello.apply(user, ["Hilton", "four stars"]);
+
+// bind
+hello(nam.increment.bind(nam));
