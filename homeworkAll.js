fdda5dce86b088fbbbb6de1a91199b4b5b852bea
Sveta2507 <dnsdns.dn@googlemail.com>
2020-08-05 17:43:46 +0300

    js

--- homeworkAll.js
+++ homeworkAll.js
@@ -164,27 +164,27 @@
 
 
 // //task 3
-// const findLongestWord = function(string) {
-//   let arr = string.split(" ");
-//   // console.log(arr);
-//   let longestWord = arr[0];
-//   for (let i = 1; i < arr.length; i++ ) {
-//     // console.log(arr[i]);
-//     if (longestWord.length < arr[i].length) {
-//       longestWord = arr[i];
-//   }
-// }
-// return longestWord;
-// };
+const findLongestWord = function(string) {
+  let arr = string.split(" ");
+  // console.log(arr);
+  let longestWord = arr[0];
+  for (let i = 1; i < arr.length; i++ ) {
+    // console.log(arr[i]);
+    if (longestWord.length < arr[i].length) {
+      longestWord = arr[i];
+  }
+}
+return longestWord;
+};
 
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
 
-// console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(findLongestWord('Google do a roll')); // 'Google'
 
-// console.log(findLongestWord('May the force be with you')); // 'force'
+console.log(findLongestWord('May the force be with you')); // 'force'
 
 
 
--- hw5.js
+++ hw5.js
@@ -2,151 +2,209 @@
 
 //module 5
 //task 1
-console.log(Account.prototype.getInfo); // function
+// function Account({login, email}) {
+//   this.login = login;
+//   this.email = email;
+// }
 
-const mango = new Account({
-  login: 'Mangozedog',
-  email: 'mango@dog.woof',
-});
+// Account.prototype.getInfo = function() {
+//     console.log(`login = ${this.login}, email = ${this.email}`);
+// }
 
-mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+// console.log(Account.prototype.getInfo); // function
 
-const poly = new Account({
-  login: 'Poly',
-  email: 'poly@mail.com',
-});
+// const mango = new Account({
+//   login: 'Mangozedog',
+//   email: 'mango@dog.woof',
+// });
 
-poly.getInfo(); // Login: Poly, Email: poly@mail.com
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account({
+//   login: 'Poly',
+//   email: 'poly@mail.com',
+// });
+
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
 
 
 
 //task 2
-const mango = new User({
-    name: 'Mango',
-    age: 2,
-    followers: 20,
-  });
+// class User {
+//   constructor(user) {
+//       this.name = user.name;
+//       this.age = user.age;
+//       this.followers = user.followers;
+//   }
+
+//   getInfo() {
+//       return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`;
+//   }
+// }
+
+// const mango = new User({
+//     name: 'Mango',
+//     age: 2,
+//     followers: 20,
+//   });
   
-  mango.getInfo(); // User Mango is 2 years old and has 20 followers
+//   console.log(mango.getInfo()); // User Mango is 2 years old and has 20 followers
   
-  const poly = new User({
-    name: 'Poly',
-    age: 3,
-    followers: 17,
-  });
+//   const poly = new User({
+//     name: 'Poly',
+//     age: 3,
+//     followers: 17,
+//   });
   
-  poly.getInfo(); // User Poly is 3 years old and has 17 followers
+//   console.log(poly.getInfo()); // User Poly is 3 years old and has 17 followers
 
 
-  //task 3
-  const storage = new Storage([
-    'Нанитоиды',
-    'Пролонгер',
-    'Железные жупи',
-    'Антигравитатор',
-  ]);
+//   //task 3
+
+// class Storage {
+//   constructor(items) {
+//       this.items = items;
+//   }
+
+//   getItems() {
+//       return this.items;
+//   }
+
+//   addItem(item) {
+//       this.items.push(item);
+//   }
+
+//   removeItem(item) {
+//       let index = this.items.indexOf(item);
+//       if (index != -1) {
+//           this.items.splice(index, 1);
+//       }
+//   }
+// }
+
+//   const storage = new Storage([
+//     'Нанитоиды',
+//     'Пролонгер',
+//     'Железные жупи',
+//     'Антигравитатор',
+//   ]);
   
-  const items = storage.getItems();
-  console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+//   const items = storage.getItems();
+//   console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
   
-  storage.addItem('Дроид');
-  console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+//   storage.addItem('Дроид');
+//   console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
   
-  storage.removeItem('Пролонгер');
-  console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+//   storage.removeItem('Пролонгер');
+//   console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
 
 
 
-  //task 4
-  const builder = new StringBuilder('.');
+//   //task 4
+// class StringBuilder {
+//   constructor(str) {
+//       this._value = str;
+//   }
 
-builder.append('^');
-console.log(builder.value); // '.^'
+//   get value() {
+//       return this._value;
+//   }
 
-builder.prepend('^');
-console.log(builder.value); // '^.^'
+//   append(str) {
+//       this._value += str;
+//   }
 
-builder.pad('=');
-console.log(builder.value); // '=^.^='
+//   prepend(str) {
+//       this._value = str + this._value;
+//   }
+
+//   pad(str) {
+//       this.prepend(str);
+//       this.append(str);
+//   }
+// }
+
+//   const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
 
 
 
-//task 5
-class Car {
-    /*
-     * Добавь статический метод `getSpecs(car)`,
-     * который принимает объект-машину как параметр и выводит
-     * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
-     */
-  
-    /*
-     * Конструктор получает объект настроек.
-     *
-     * Добавь свойства будущеего экземпляра класса:
-     *  speed - текущая скорость, изначально 0
-     *  price - цена автомобиля
-     *  maxSpeed - максимальная скорость
-     *  isOn - заведен ли автомобиль, значения true или false. Изначально false
-     *  distance - общий киллометраж, изначально 0
-     */
-    constructor() {}
-  
-    /*
-     * Добавь геттер и сеттер для свойства price,
-     * который будет работать с свойством цены автомобиля.
-     */
-  
-    /*
-     * Добавь код для того чтобы завести автомобиль
-     * Записывает в свойство isOn значение true
-     */
-    turnOn() {}
-  
-    /*
-     * Добавь код для того чтобы заглушить автомобиль
-     * Записывает в свойство isOn значение false,
-     * и сбрасывает текущую скорость в 0
-     */
-    turnOff() {}
-  
-    /*
-     * Добавялет к свойству speed полученное значение,
-     * при условии что результирующая скорость
-     * не больше чем значение свойства maxSpeed
-     */
-    accelerate(value) {}
-  
-    /*
-     * Отнимает от свойства speed полученное значение,
-     * при условии что результирующая скорость не меньше нуля
-     */
-    decelerate(value) {}
-  
-    /*
-     * Добавляет в поле distance киллометраж (hours * speed),
-     * но только в том случае если машина заведена!
-     */
-    drive(hours) {}
-  }
-  
-  const mustang = new Car({ maxSpeed: 200, price: 2000 });
-  
-  mustang.turnOn();
-  mustang.accelerate(50);
-  mustang.drive(2);
-  
-  Car.getSpecs(mustang);
-  // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
-  
-  mustang.decelerate(20);
-  mustang.drive(1);
-  mustang.turnOff();
-  
-  Car.getSpecs(mustang);
-  // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+// //task 5
+// class Car {
+//   static getSpecs(car) {
+//     console.log(`maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`);
+//   }
 
-console.log(mustang.price); // 2000
-mustang.price = 4000;
-console.log(mustang.price); // 4000
+//     constructor(car) {
+//       this.speed = 0;
+//       this._price = car.price;
+//       this.maxSpeed = car.maxSpeed;
+//       this.isOn = false;
+//       this.distance = 0;
+//     }
 
-ss
+//     get price() {
+//       return this._price;
+//     }
+
+//     set price(value) {
+//       this._price = value;
+//     }
+
+//     turnOn() {
+//       return this.isOn = true;
+//     }
+  
+
+//     turnOff() {
+//       return this.isOn = false, this.speed = 0;
+//     }
+  
+//     accelerate(value) {
+//       this.speed += value;
+//       if (this.speed <= this.maxSpeed && this.isOn) return this.speed;
+//     }
+  
+//     decelerate(value) {
+//       this.speed -= value;
+//       if (this.speed > 0) return this.speed;
+//     }
+  
+//     /*
+//      * Добавляет в поле distance киллометраж (hours * speed),
+//      * но только в том случае если машина заведена!
+//      */
+//     drive(hours) {
+//       this.distance += hours * this.speed;
+//       console.log(this.distance);
+//       if (this.isOn) return this.distance;
+//   }
+// }
+  
+//   const mustang = new Car({ maxSpeed: 200, price: 2000 });
+  
+//   mustang.turnOn();
+//   mustang.accelerate(50);
+//   mustang.drive(2);
+  
+//   Car.getSpecs(mustang);
+//   // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+  
+//   mustang.decelerate(20);
+//   mustang.drive(1);
+//   mustang.turnOff();
+  
+//   Car.getSpecs(mustang);
+//   // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
