6793c952f588c8df06f2a59a7631064cdc8a51d4
Sveta2507 <dnsdns.dn@googlemail.com>
2020-08-05 09:23:35 +0300

    js

--- hw4.js
+++ hw4.js
@@ -1,30 +1,30 @@
 // module 4
 //task 1
-// const account = {
-//     owner: 'Mango',
-//     balance: 24000,
-//     discount: 0.1,
-//     orders: ['order-1', 'order-2', 'order-3'],
-//     changeDiscount(value) {
-//       discount = value;
-//     },
-//     showOrders() {
-//       return this.orders;
-//     },
-//     addOrder(cost, order) {
-//       this.balance -= cost;
-//       this.orders.push(order);
-//     },
-//   };
+const account = {
+    owner: 'Mango',
+    balance: 24000,
+    discount: 0.1,
+    orders: ['order-1', 'order-2', 'order-3'],
+    changeDiscount(value) {
+      this.discount = value;
+    },
+    showOrders() {
+      return this.orders;
+    },
+    addOrder(cost, order) {
+      this.balance -= cost;
+      this.orders.push(order);
+    },
+  };
   
-//   account.changeDiscount(0.15);
-//   console.log(account.discount); // 0.15
+  account.changeDiscount(0.15);
+  console.log(account.discount); // 0.15
   
-//   console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
   
-//   account.addOrder(5000, 'order-4');
-//   console.log(account.balance); // 19000
-//   console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+  account.addOrder(5000, 'order-4');
+  console.log(account.balance); // 19000
+  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
 
 
 //task 2
@@ -33,7 +33,7 @@ const inventory = {
     add(itemName) {
       console.log(`Adding ${itemName} to inventory`);
   
-      inventory.items.push(itemName);
+      this.items.push(itemName);
     },
     remove(itemName) {
       console.log(`Removing ${itemName} from inventory`);
@@ -47,13 +47,13 @@ const inventory = {
     action(itemName);
   };
   
-  invokeInventoryAction('Medkit', inventory.add);
+  invokeInventoryAction('Medkit', inventory.add.bind(inventory));
   // Invoking action on Medkit
   // Adding Medkit to inventory
   
   console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
   
-  invokeInventoryAction('Gas mask', inventory.remove);
+  invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
   // Invoking action on Gas mask
   // Removing Gas mask from inventory
   
--- hw5.js
+++ hw5.js
@@ -2,3 +2,151 @@
 
 //module 5
 //task 1
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+
+
+//task 2
+const mango = new User({
+    name: 'Mango',
+    age: 2,
+    followers: 20,
+  });
+  
+  mango.getInfo(); // User Mango is 2 years old and has 20 followers
+  
+  const poly = new User({
+    name: 'Poly',
+    age: 3,
+    followers: 17,
+  });
+  
+  poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+
+  //task 3
+  const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+  ]);
+  
+  const items = storage.getItems();
+  console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+  
+  storage.addItem('Дроид');
+  console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+  
+  storage.removeItem('Пролонгер');
+  console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+
+
+  //task 4
+  const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
+
+
+
+//task 5
+class Car {
+    /*
+     * Добавь статический метод `getSpecs(car)`,
+     * который принимает объект-машину как параметр и выводит
+     * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+     */
+  
+    /*
+     * Конструктор получает объект настроек.
+     *
+     * Добавь свойства будущеего экземпляра класса:
+     *  speed - текущая скорость, изначально 0
+     *  price - цена автомобиля
+     *  maxSpeed - максимальная скорость
+     *  isOn - заведен ли автомобиль, значения true или false. Изначально false
+     *  distance - общий киллометраж, изначально 0
+     */
+    constructor() {}
+  
+    /*
+     * Добавь геттер и сеттер для свойства price,
+     * который будет работать с свойством цены автомобиля.
+     */
+  
+    /*
+     * Добавь код для того чтобы завести автомобиль
+     * Записывает в свойство isOn значение true
+     */
+    turnOn() {}
+  
+    /*
+     * Добавь код для того чтобы заглушить автомобиль
+     * Записывает в свойство isOn значение false,
+     * и сбрасывает текущую скорость в 0
+     */
+    turnOff() {}
+  
+    /*
+     * Добавялет к свойству speed полученное значение,
+     * при условии что результирующая скорость
+     * не больше чем значение свойства maxSpeed
+     */
+    accelerate(value) {}
+  
+    /*
+     * Отнимает от свойства speed полученное значение,
+     * при условии что результирующая скорость не меньше нуля
+     */
+    decelerate(value) {}
+  
+    /*
+     * Добавляет в поле distance киллометраж (hours * speed),
+     * но только в том случае если машина заведена!
+     */
+    drive(hours) {}
+  }
+  
+  const mustang = new Car({ maxSpeed: 200, price: 2000 });
+  
+  mustang.turnOn();
+  mustang.accelerate(50);
+  mustang.drive(2);
+  
+  Car.getSpecs(mustang);
+  // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+  
+  mustang.decelerate(20);
+  mustang.drive(1);
+  mustang.turnOff();
+  
+  Car.getSpecs(mustang);
+  // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
+
+ss
