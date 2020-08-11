be142367e4a872a53384549ef1d51b123bb34492
Sveta2507 <dnsdns.dn@googlemail.com>
2020-08-11 23:49:50 +0300

    js

--- hw7.js
+++ hw7.js
@@ -1,34 +1,65 @@
 // task 1
+const mainUl = document.getElementById("categories");
+console.dir(mainUl);
+
+console.log(`в списке ${mainUl.children.length} категорий`);
+
+for (let i = 0; i < mainUl.children.length; i++) {
+  const li = mainUl.children[i];
+  console.log(li.children[0].textContent);
+  console.dir(`${li.children[1].children.length} вложеных элементов`);
+}
 
 // task 2
 const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
-  ];
-
-  // task 3
-  
-  const images = [
-    {
-      url:
-        'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'White and Black Long Fur Cat',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Group of Horses Running',
-    },
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
 
+const ingr = document.getElementById("ingredients");
+const createElements = function (arr) {
+  for (let element of arr) {
+    const li = document.createElement("li");
+    li.textContent = element;
+    ingr.appendChild(li);
+  }
+};
+createElements(ingredients);
+
+// task 3
+
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const gallery = document.getElementById("gallery");
+const createItems = function (arr) {
+  for (let elem of arr) {
+    console.log(elem.url);
+    const img = `<img src="${elem.url}">`;
+    gallery.insertAdjacentHTML("afterbegin", img);
+  }
+};
+createItems(images);
 
 //task 4
+
+
--- module7.js
+++ module7.js
@@ -43,3 +43,66 @@ newLi.remove(); // новый метод
 
 const newImage = image.cloneNode(false);
 list.insertAdjacentElement("afterend", newImage);
+
+
+const ul = `<ul><li>element</li></ul>`;
+console.dir(ul);
+
+const body = document.querySelector("body");
+
+body.insertAdjacentHTML( "afterbegin",ul);
+
+const div = document.querySelector(".div");
+console.log(div);
+
+div.style.border = "1px solid red";
+
+const link1 = document.createElement("a");
+const link2 = document.createElement("a");
+const link3 = document.createElement("a");
+const link4 = document.createElement("a");
+ link1.textContent = "before begin";
+ link2.textContent = "before end";
+ link3.textContent = "after end";
+ link4.textContent = "after begin";
+
+ div.insertAdjacentElement("beforebegin", link1);
+ div.insertAdjacentElement("beforeend", link2);
+ div.insertAdjacentElement("afterend", link3);
+ div.insertAdjacentElement("afterbegin", link4);
+
+// div.remove(link2);
+
+link1.remove();
+link2.replaceWith(link1);
+
+div.innerHTML = "";
+
+// repaint & reflow
+
+const button = document.querySelector("button");
+button.textContent = "click me";
+button.style.width = "100px";
+button.style.height = "50px";
+button.style.backgroundColor = "blue";
+button.addEventListener("click", (event) => {
+    console.log(event.target);
+    console.log(event.currentTarget);
+    console.log("СРАБОТАЛО СОБЫТИЕ КЛИК");
+    const picture = document.createElement("img");
+    picture.setAttribute("src", "https://images.pexels.com/photos/2191673/pexels-photo-2191673.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
+    div.append(picture);
+});
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("произошла отправка формы");
+    console.dir(event);
+});
+
+body.addEventListener("click", (event) => {
+    console.log(event.target);
+    console.log(event.currentTarget);
+});
